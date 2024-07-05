4e07e6ed640478ca7b25e1be543c98c8172ea0ae
Parneet_uppal <parneetuppal7053@gmail.com>
2024-07-05 14:11:11 -0500

    Made Public

---
+++ Password_encrypt/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Password_encrypt
+{
+    partial class Password_Encrypt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtresult = new System.Windows.Forms.TextBox();
+            this.lblEncrypt = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnreset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(41, 41);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(81, 13);
+            this.lblPassword.TabIndex = 0;
+            this.lblPassword.Text = "Your Password ";
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(151, 34);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(230, 20);
+            this.txtpassword.TabIndex = 1;
+            // 
+            // txtresult
+            // 
+            this.txtresult.Location = new System.Drawing.Point(151, 60);
+            this.txtresult.Name = "txtresult";
+            this.txtresult.Size = new System.Drawing.Size(230, 20);
+            this.txtresult.TabIndex = 3;
+            // 
+            // lblEncrypt
+            // 
+            this.lblEncrypt.AutoSize = true;
+            this.lblEncrypt.Location = new System.Drawing.Point(41, 67);
+            this.lblEncrypt.Name = "lblEncrypt";
+            this.lblEncrypt.Size = new System.Drawing.Size(104, 13);
+            this.lblEncrypt.TabIndex = 2;
+            this.lblEncrypt.Text = "Encrypted Password";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(289, 98);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 4;
+            this.btnEncrypt.Text = "ENCRYPT";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // btnreset
+            // 
+            this.btnreset.Location = new System.Drawing.Point(151, 98);
+            this.btnreset.Name = "btnreset";
+            this.btnreset.Size = new System.Drawing.Size(75, 23);
+            this.btnreset.TabIndex = 5;
+            this.btnreset.Text = "RESET";
+            this.btnreset.UseVisualStyleBackColor = true;
+            // 
+            // Password_Encrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 133);
+            this.Controls.Add(this.btnreset);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtresult);
+            this.Controls.Add(this.lblEncrypt);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.lblPassword);
+            this.Name = "Password_Encrypt";
+            this.Text = "Password Encrypter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtresult;
+        private System.Windows.Forms.Label lblEncrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnreset;
+    }
+}
+
---
+++ Password_encrypt/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Password_encrypt
+{
+    public partial class Password_Encrypt : Form
+    {
+        public Password_Encrypt()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+            this.btnEncrypt.Click += btnEncrypt_Click;
+            this.btnreset.Click += Btnreset_Click;
+        }
+
+        private void Btnreset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        static string Encrypt(string value)
+        {
+            //Using MD5 to encrypt a string
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                UTF8Encoding utf8 = new UTF8Encoding();
+                //Hash data
+                byte[] data = md5.ComputeHash(utf8.GetBytes(value));
+                return Convert.ToBase64String(data);
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtpassword.Text))
+            {
+                MessageBox.Show("Please enter your password.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            txtresult.Text = Encrypt(txtpassword.Text);
+        }
+
+        private void Reset()
+        {
+            this.txtpassword.Text = string.Empty;
+            this.txtresult.Text = string.Empty;
+        }
+    }
+}
+ 
